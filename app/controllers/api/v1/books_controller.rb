17a68437ac027c004dcf3ca186af0b97c1703294
pcbennett108 <pcbennett42@gmail.com>
2024-01-16 11:05:04 -0700

    Add books serializer

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BooksController < ApplicationController
   def search
     facade = BooksFacade.new.search_books(params[:location], params[:quantity])
+    # require "pry" ; binding.pry
     render json: BooksSerializer.new(facade)
   end
 end
--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -3,6 +3,6 @@ class BooksFacade
     book_data = BooksService.new.get_books(location, quantity)
     weather_data = WeatherFacade.new(location).forecast
     Book.new(location, weather_data, book_data)
-    require "pry" ; binding.pry
+    # require "pry" ; binding.pry
   end
 end
