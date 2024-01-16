a6770f15c1aadc956314462bd0667ea2edd82a26
pcbennett108 <pcbennett42@gmail.com>
2024-01-16 11:37:01 -0700

    Add format weather method to bok poro

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -8,8 +8,17 @@ class Book
   def initialize(destination, weather_data, book_data)
     @id = nil
     @destination = destination
-    @forecast = weather_data.current
+    @forecast = format_weather(weather_data.current)
     @total_books_found = book_data[:numFound]
     @books = book_data[:docs]
   end
+end
+
+private
+
+def format_weather(weather)
+  {
+    summary: weather[:condition],
+    temperature: "#{weather[:temperature]} °F"
+  }
 end
