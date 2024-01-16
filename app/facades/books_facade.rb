0cee28b89690c76146112bb33a6352a18387d8a6
pcbennett108 <pcbennett42@gmail.com>
2024-01-16 10:29:57 -0700

    Collect weather info in books facade

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -1,5 +1,7 @@
 class BooksFacade
   def search_books(location, quantity)
-    book = BooksService.new.get_books(location, quantity)
+    book_data = BooksService.new.get_books(location, quantity)
+    weather_data = WeatherFacade.new(location).forecast
+    # require "pry" ; binding.pry
   end
 end
